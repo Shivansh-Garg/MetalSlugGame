1134320d62a32867819b4a83aa8138ee73fb9dd6
Keshav Rawat <60732963+kshavv@users.noreply.github.com>
2024-09-28 07:40:51 +0530

    added assets for level design

--- Assets/Scripts/Enemy/RangedEnemy.cs
+++ Assets/Scripts/Enemy/RangedEnemy.cs
@@ -118,7 +118,7 @@ public class RangedEnemy : MonoBehaviour
             {
                 anime.SetTrigger("died");
 
-
+                
                 Destroy(gameObject);
                 _isDead = true;
             }
